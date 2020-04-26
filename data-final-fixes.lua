b2cc7736ba85508f3cc9623337c8d03309e7c7ff
Andreas Wälchli <andreas.waelchli@me.com>
2020-04-26 18:13:00 +0200

    barrels-of-steam integration

---
+++ data-final-fixes.lua
@@ -0,0 +1,12 @@
+local function patchBarrel(barrel, energy)
+	if barrel then
+		if not barrel.fuel_category then
+			barrel.fuel_category = "SteamTrains-steam"
+			barrel.fuel_value = energy
+			barrel.burnt_result = "empty-barrel"
+		end
+	end
+end
+
+patchBarrel(data.raw["item"]["steam-barrel-165"], "1500kJ")
+patchBarrel(data.raw["item"]["steam-barrel-500"], "4850kJ")
