5b028f902c1d979cd305b250abdc92b729519863
main() <main@ehvag.de>
2022-08-29 23:24:05 +0200

    fix indent

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -6,10 +6,10 @@ local locoRecipe = {
     ingredients =
     {
       {"steam-engine", 1},
-	  {"storage-tank", 1},
+	    {"storage-tank", 1},
       {"electronic-circuit", 10},
       {"steel-plate", 30},
-	  {"iron-gear-wheel", 10}, 
+	    {"iron-gear-wheel", 10},
     },
     result = "SteamTrains-locomotive"
 }
